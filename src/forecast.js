ed770ac918be01ab9aeebab85356783db3b62d11
Marcus Lynch <marcusjameslynch@gmail.com>
2019-03-20 15:11:06 +0000

    added high temp

--- src/forecast.js
+++ src/forecast.js
@@ -3,13 +3,13 @@ const request = require('request');
 const forecast = (lat, long, callback) => {
     const url = `https://api.darksky.net/forecast/90f685f90029681d6ed9cb4461c2b876/${lat},${long}`;
 
-    request({url, json: true}, (error, {body}) => {
+    request({url, json: true}, (error, {body}) => {        
         if (error) {
             callback("unable to connect to weather forecast", undefined);
         } else if (body.error) {
             callback("Unable to find location, try another search", undefined);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. High temp is : ${body.daily.data[0].temperatureHigh}`)
         }
     })
 }
