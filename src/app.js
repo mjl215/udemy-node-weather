64ff6a223e77593807457077ef40c8ed2c8df180
Marcus Lynch <marcusjameslynch@gmail.com>
2019-03-20 14:52:00 +0000

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const geocode = require('./geocode');
 const forecast = require('./forecast');
 
 const app = express();
-
+// heroku 
+const port = process.env.PORT || 3000; 
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -110,6 +111,6 @@ app.get('*', (req, res) =>{
 // app.com/help
 // app.com/about
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000');
+app.listen(port, () => {
+    console.log('server is up on port ' + port);
 });
